a4766036e814627488dec3da3d39665344924eae
neojarvis <techadmin@iamneo.ai>
2024-01-08 05:30:52 +0000

    Auto-save-commit-1/8/2024--11:00:44-AM

---
+++ CheckDebugConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int total=0;
+Console.WriteLine("Num1");
+int a= Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Num2");
+int b= Convert.ToInt32(Console.ReadLine());
+total =a+b;
+System.Console.WriteLine($"Total {total}");
